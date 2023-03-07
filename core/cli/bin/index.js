2adc441febe38f5e60e57657c69692799f239406
zhb333 <1140457303@qq.com>
2023-03-08 02:57:33 +0800

    chore

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -3,7 +3,7 @@
 const importLocal = require("import-local");
 
 if (importLocal(__filename)) {
-  require("npmlog").info("cli", "正在使用 knzn-cli 本地版本");
+  require("npmlog").info("cli", "正在使用 knzn-cli 本地版本 !");
 } else {
   require("../lib/cli.js")();
 }
